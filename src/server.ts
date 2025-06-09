ea578fb4e996f00802bf1780cef8886bd529a583
RenanTheFato <renan.santana007@hotmail.com>
2025-06-09 11:13:24 -0300

    configuração inical do servidor

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import fastifyCors from "@fastify/cors";
+import { fastify } from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = fastify({ logger: true })
+
+const server = async () => {
+  await app.register(fastifyCors)
+
+  const SERVER_HOST = process.env.HTTP_HOST
+  const SERVER_PORT = process.env.HTTP_PORT
+
+  await app.listen({
+    host: String(SERVER_HOST),
+    port: Number(SERVER_PORT),
+  }).then(() => {
+    console.log(`[SREVER] Servidor HTTP rodando na porta ${SERVER_PORT}`)
+  }).catch((error) => {
+    console.log(`[ERROR] Erro ao iniciar o servidor HTTP => ${error}`)
+    process.exit(1)
+  })
+}
+
+server()
