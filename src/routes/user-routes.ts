46d5a1b12f434c31344006366c559fe4cebcc0aa
RenanTheFato <renan.santana007@hotmail.com>
2025-06-10 10:50:27 -0300

    correção: mudado o tipo da rota /cadastro de GET para POST, para ficar de acordo com o requerido, GET estava sendo utilizado para testes na lógica de validação do CPF

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { CadastrarUsuarioController } from "../controllers/usuarios/cadastrar.usuario.controller.js";
 
 export async function userRoutes(fastify: FastifyInstance) {
-  fastify.get("/cadastro", async(req: FastifyRequest, rep: FastifyReply) => {
+  fastify.post("/cadastro", async(req: FastifyRequest, rep: FastifyReply) => {
     return new CadastrarUsuarioController().handle(req, rep)
   })
 
